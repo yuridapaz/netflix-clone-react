ece9f1e0fd794c901279d4868eefb91b8f6ddeb7
yuridapaz <yuridapaz@gmail.com>
2024-01-13 18:29:31 -0300

    button format

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,49 @@
 import React from 'react';
+import Button from './components/button';
 
 function App() {
   return (
     <>
       <h1>New Site</h1>
+      <h6>button test</h6>
+      <div className="flex flex-col w-full gap-6 bg-slate-700 p-6">
+        <Button variant="primary" size="small">
+          Primary S
+        </Button>
+        <Button variant="primary" size="medium">
+          Primary M
+        </Button>
+        <Button variant="primary" size="large">
+          Primary L
+        </Button>
+        <Button variant="secondary" size="small">
+          Secondary S
+        </Button>
+        <Button variant="secondary" size="medium">
+          Secondary M
+        </Button>
+        <Button variant="secondary" size="large">
+          Secondary L
+        </Button>
+        <Button variant="red" size="small">
+          Red Button S
+        </Button>
+        <Button variant="red" size="medium">
+          Red Button M
+        </Button>
+        <Button variant="red" size="large">
+          Red Button L
+        </Button>
+        <Button variant="outline" size="small">
+          Outlined S
+        </Button>
+        <Button variant="outline" size="medium">
+          Outlined M
+        </Button>
+        <Button variant="outline" size="large">
+          Outlined L
+        </Button>
+      </div>
     </>
   );
 }
--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,25 +1,26 @@
-import { cva } from 'class-variance-authority';
+import { VariantProps, cva } from 'class-variance-authority';
 import React, { ButtonHTMLAttributes, FC } from 'react';
 
-type buttonProps = Pick<
-  ButtonHTMLAttributes<HTMLButtonElement>,
-  'type' | 'disabled' | 'onClick' | 'children' | 'className'
-> & {
-  'data-testid'?: string;
-};
+type buttonProps = VariantProps<typeof buttonVariants> &
+  Pick<ButtonHTMLAttributes<HTMLButtonElement>, 'disabled' | 'onClick' | 'children' | 'className'> & {
+    'data-testid'?: string;
+  };
 
-const buttonVariants = cva('transition-all', {
+const buttonVariants = cva('transition-all m-auto', {
   variants: {
     variant: {
-      primary: '',
-      secondary: '',
-      outline: '',
-      red: '',
+      primary: 'bg-none text-white',
+      secondary: 'bg-white text-black',
+      outline: 'bg-none text-gray-300 outline outline-1 outline-gray-300',
+      red: 'bg-red-600 text-white rounded-md',
     },
     size: {
-      small: '',
-      medium: '',
-      large: '',
+      small: 'p-2',
+      medium: 'p-3',
+      large: 'p-4',
+    },
+    bold: {
+      true: 'font-bold',
     },
   },
 });
@@ -30,12 +31,15 @@ const Button: FC<buttonProps> = ({
   children,
   className,
   'data-testid': dataTestId,
+  variant,
+  size,
+  bold,
 }) => {
   return (
     <button
       disabled={disabled}
       onClick={onClick}
-      className={className}
+      className={buttonVariants({ variant, size, bold, className })}
       data-testid={dataTestId}
     >
       {children}
