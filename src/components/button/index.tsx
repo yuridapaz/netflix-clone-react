6e21948527d810a9defaa9fcbef87e041a76999e
yuridapaz <yuridapaz@gmail.com>
2024-01-13 19:02:25 -0300

    text input style

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -2,17 +2,20 @@ import { VariantProps, cva } from 'class-variance-authority';
 import React, { ButtonHTMLAttributes, FC } from 'react';
 
 type buttonProps = VariantProps<typeof buttonVariants> &
-  Pick<ButtonHTMLAttributes<HTMLButtonElement>, 'disabled' | 'onClick' | 'children' | 'className'> & {
+  Pick<
+    ButtonHTMLAttributes<HTMLButtonElement>,
+    'disabled' | 'onClick' | 'children' | 'className'
+  > & {
     'data-testid'?: string;
   };
 
-const buttonVariants = cva('transition-all m-auto', {
+const buttonVariants = cva('transition-all m-auto cursor-pointer', {
   variants: {
     variant: {
       primary: 'bg-none text-white',
-      secondary: 'bg-white text-black',
+      secondary: 'bg-white text-black hover:bg-white/90',
       outline: 'bg-none text-gray-300 outline outline-1 outline-gray-300',
-      red: 'bg-red-600 text-white rounded-md',
+      red: 'bg-red-600 text-white rounded-md hover:bg-red-600/90',
     },
     size: {
       small: 'p-2',
@@ -22,6 +25,10 @@ const buttonVariants = cva('transition-all m-auto', {
     bold: {
       true: 'font-bold',
     },
+  },
+  defaultVariants: {
+    variant: 'primary',
+    size: 'medium',
   },
 });
 
