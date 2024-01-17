54b53657ea273eb48c9b5af54f51ddae7fe3af24
yuridapaz <yuridapaz@gmail.com>
2024-01-17 17:32:02 -0300

    request movies

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from './pages/errorPage/index';
+import Home from './pages/home/index.js';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+    ],
+  },
+
+  //TODO: Sign in
+  //TODO: Sign up
+  //TODO: Movie Description ? Maybe an modal.
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
