19f23f4b6e2514ff73b0697f8c00ec6b0e27ab04
yuridapaz <yuridapaz@gmail.com>
2024-01-18 15:08:37 -0300

    row slider

--- src/components/rowComponent/index.tsx
+++ src/components/rowComponent/index.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import MovieCard from '../movieCard';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
 const RowComponent = ({ rowTitle, fetchURL }) => {
   const [movies, setMovies] = useState([]);
@@ -11,10 +12,24 @@ const RowComponent = ({ rowTitle, fetchURL }) => {
     });
   }, [fetchURL]);
 
+  const scrollLeft = () => {
+    let slider = document.getElementById('slider');
+    slider.scrollLeft = slider.scrollLeft - 200;
+  };
+  const scrollRight = () => {
+    let slider = document.getElementById('slider');
+    slider.scrollLeft = slider.scrollLeft + 200;
+  };
+
   return (
     <>
       <h1 className="p-4 font-bold text-white md:text-xl">{rowTitle}</h1>
-      <div className="relative flex items-center">
+      <div className="group relative flex items-center">
+        <MdChevronLeft
+          onClick={scrollLeft}
+          size={35}
+          className="absolute z-10 hidden h-full  cursor-pointer bg-black text-white opacity-50 hover:opacity-70 group-hover:block"
+        />
         <div
           id={`slider`}
           className="no-scrollbar relative h-full w-full overflow-scroll scroll-smooth whitespace-nowrap"
@@ -23,6 +38,11 @@ const RowComponent = ({ rowTitle, fetchURL }) => {
             <MovieCard movie={movie} key={id} />
           ))}
         </div>
+        <MdChevronRight
+          onClick={scrollRight}
+          size={35}
+          className="absolute right-0 z-10 hidden  h-full cursor-pointer bg-black text-white opacity-50 hover:opacity-70 group-hover:block"
+        />
       </div>
     </>
   );
