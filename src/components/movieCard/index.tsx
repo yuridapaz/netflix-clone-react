d7c150358f640d68ee4f758cbd7fdd2943cdddea
yuridapaz <yuridapaz@gmail.com>
2024-01-18 14:29:36 -0300

    movie card heart icon

--- src/components/movieCard/index.tsx
+++ src/components/movieCard/index.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import { FaHeart, FaRegHeart } from 'react-icons/fa';
 
-const MovieCard = () => {
+const MovieCard = ({ movie }) => {
+  const like = false;
   return (
     <div className="relative inline-block w-[160px] cursor-pointer sm:w-[200px] md:w-[240px] lg:w-[280px]">
-      <img src="" alt="" className="block h-auto w-full" />
+      <img
+        src={`https://image.tmdb.org/t/p/w500/${movie?.backdrop_path}`}
+        alt={movie?.title}
+        className="block h-auto w-full"
+      />
       <div className="absolute left-0 top-0 h-full w-full text-white opacity-0 transition-all  hover:bg-black/75 hover:opacity-100">
-        <p className="flex h-full items-center justify-center whitespace-normal text-xs font-bold md:text-sm">
-          Interstellar
+        <p className="flex h-full items-center justify-center whitespace-normal text-xs font-bold  md:text-sm">
+          {movie?.title}
         </p>
-        <p className="absolute right-2 top-2 md:right-4 md:top-4">🩶</p>
+        <p className="absolute right-2 top-2 md:right-4 md:top-4">
+          {like ? <FaHeart /> : <FaRegHeart />}
+        </p>
       </div>
     </div>
   );
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,33 @@
 import React, { useEffect } from 'react';
 import MainComponent from '../../components/main';
-import requests from '../../utils/request';
-import axios from 'axios';
+import MovieCard from '../../components/movieCard';
+
+const filme = {
+  adult: false,
+  backdrop_path: '/f1AQhx6ZfGhPZFTVKgxG91PhEYc.jpg',
+  genre_ids: [36, 10752, 18],
+  id: 753342,
+  original_language: 'en',
+  original_title: 'Napoleon',
+  overview:
+    'An epic that details the checkered rise and fall of French Emperor Napoleon Bonaparte and his relentless journey to power through the prism of his addictive, volatile relationship with his wife, Josephine.',
+  popularity: 2367.318,
+  poster_path: '/jE5o7y9K6pZtWNNMEw3IdpHuncR.jpg',
+  release_date: '2023-11-22',
+  title: 'Napoleon',
+  video: false,
+  vote_average: 6.519,
+  vote_count: 1226,
+};
 
 const Home = () => {
-  useEffect(() => {}, []);
-
   return (
     <div className="h-full min-h-screen w-full">
       <MainComponent />
+      <h1>oi</h1>
+      <MovieCard movie={filme} />
+      <MovieCard movie={filme} />
+      <MovieCard movie={filme} />
     </div>
   );
 };
