13be1ed8abf2db6f0674da747de03d5957d8fa8d
yuridapaz <yuridapaz@gmail.com>
2024-01-20 15:27:02 -0300

    up

--- src/components/navBar/index.tsx
+++ src/components/navBar/index.tsx
@@ -3,7 +3,7 @@ import Button from '../button';
 
 const NavBar = () => {
   return (
-    <div className="absolute z-[100] flex w-full items-center justify-between p-4">
+    <div className="absolute z-[100] flex w-full items-center justify-between p-4 md:px-8">
       <h1 className="cursor-pointer text-4xl font-normal uppercase text-red-600">Netflix</h1>
       <div className="flex gap-4">
         <Button>Sign In</Button>
